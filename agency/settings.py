98f0bbe3f5dd930e51944c4e34b2e6909e7a55d1
COLLINCE <omondicolli@gmail.com>
2025-06-17 21:50:15 +0300

    Removing unnecessary settings.py

--- agency/settings.py
+++ agency/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'], cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Add CSRF_TRUSTED_ORIGINS
